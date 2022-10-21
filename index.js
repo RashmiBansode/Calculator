102aead8eb75f71958391a7b1d076255877cc160
RashmiBansode <rashmibansode86@gmail.com>
2022-10-21 15:48:34 +0530

    commited

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function add(){
+let a=parseInt(document.getElementById('num1').value);
+let b=parseInt(document.getElementById('num2').value);
+let total=a+b;
+document.write("Addition of Two No. is =  "+total)
+}
+
+function substract(){
+    let a= document.getElementById('num1').value;
+let b= document.getElementById('num2').value;
+let total=a-b;
+document.write("Substraction of Two No. is =  "+total)
+}
+
+
+function mult(){
+    let a= document.getElementById('num1').value;
+let b= document.getElementById('num2').value;
+let total=a*b;
+document.write("Multiplication of Two No. is=   "+total)
+}
+
+
+function div(){
+    let a= document.getElementById('num1').value;
+let b= document.getElementById('num2').value;
+let total=a/b;
+document.write("Division of Two No. is =   "+total)
+}
+
